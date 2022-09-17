ba7dd68f536bb282e7e3766b35387888f5b09a77
andersonlietz <81104621+andersonlietz@users.noreply.github.com>
2022-09-16 21:54:44 -0300

    Modificações iniciais finalizadas

--- footer.php
+++ footer.php
@@ -1,5 +1,2 @@
-
-
-
-<div id="copy">&copy; 2022 - lietz.com.br - Todos os direitos reservados.
+<div id="copy">&copy; 2022 - La Belle de Jour - Todos os direitos reservados.
     <br> Desenvolvido por <a href="https://lietz.dev.br">Lietz Desenvolvimento</a></div>
--- index.php
+++ index.php
@@ -1,6 +1,44 @@
-<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="css/style.css"><link rel="shortcut icon" href="favicon-16x16.png" type="image x-icon"><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Bel Lietz cabeleireira</title><meta name="description" content="Bel Lietz, Cabeleireira em Cambé PR" /></head><body><header><h1>Bel Cabeleireira</h1></header><main><article><h2>Cabeleireira em Cambé PR</h2><p>Trabalhamos com variados tipos de cortes e penteados, levando em conta as preferências da cliente.</p><section id="amostras"><img src="amostra1.jpg"><img src="amostra2.jpg"><img src="amostra3.jpg"><img src="amostra4.jpg"></section></article><aside><h2>📌Endereço</h2><p>📍 Maria Jacomel Pacolla, 1249
-            <br>Jardim Ana Rosa, Cambé, PR
-            <img src="banner.jpg"></p></aside><section id="contato"><a href="https://wa.me/5541997739364"><img src="whatsapp.png"></a></section></main><footer>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="shortcut icon" href="favicon-16x16.png" type="image x-icon">
+        <meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>La Belle de Jour</title>
+        <meta name="description" content="Bonecos de pano com as características suas ou de quem você for presentear! " />
+    </head>
+    <body>
+        <header>
+            <h1>
+                La Belle de Jour
+            </h1>
+        </header>
+        <main>
+            <article>
+                <h2>
+                    Bonecos Artesanais!
+                </h2>
+                <p>Bonecos de pano com as características suas ou de quem você for presentear! </p>
+                <section id="amostras">
+                    <img src="imagens/amostra1.jpg">
+                    <img src="imagens/amostra2.jpg">
+                    <img src="imagens/amostra3.jpg">
+                    <img src="imagens/amostra4.jpg">
+                </section>
+            </article>
+            <aside>
+                <h2>Faça já sua encomenda!</h2>
+                <p>Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui 
+                    Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui Texto aqui</p>
+        </aside>
+        <section id="contato">
+            <a href="https://wa.me/##########"><img src="whatsapp.png"></a>
+        </section>
+    </main>
+    <footer>
                 <?php
 require "footer.php";
-?></footer></body></html> 
+?></footer>
+</body>
+</html> 
